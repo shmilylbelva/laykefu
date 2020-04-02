a46fb87e154c3ac753b6fd821cd711dbaf074508
shmilylbelva <1028604181@qq.com>
2020-04-02 16:25:55 +0800

    优化配置项

--- application/database.php
+++ application/database.php
@@ -12,27 +12,27 @@ use think\Env;
 
 return [
     // 数据库类型
-    'type'            => Env::get('database.type', 'mysql'),
+    'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => Env::get('database.hostname', '127.0.0.1'),
+    'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => Env::get('database.database', 'laykefu'),
+    'database'        => 'laykefu',
     // 用户名
-    'username'        => Env::get('database.username', 'root'),
+    'username'        => 'laykefu',
     // 密码
-    'password'        => Env::get('database.password', 'root'),
+    'password'        => '123456',
     // 端口
-    'hostport'        => Env::get('database.hostport', '3306'),
+    'hostport'        =>  '3306',
     // 连接dsn
     'dsn'             => '',
     // 数据库连接参数
     'params'          => [],
     // 数据库编码默认采用utf8
-    'charset'         => Env::get('database.charset', 'utf8'),
+    'charset'         => 'utf8',
     // 数据库表前缀
-    'prefix'          => Env::get('database.prefix', 'ws_'),
+    'prefix'          => 'ws_',
     // 数据库调试模式
-    'debug'           => Env::get('database.debug', true),
+    'debug'           =>  true,
     // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
     'deploy'          => 0,
     // 数据库读写是否分离 主从式有效
