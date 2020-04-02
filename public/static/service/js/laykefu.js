d1d6dc990979f054d53c33f4eea83f2b4bda6787
shmilylbelva <1028604181@qq.com>
2020-04-02 16:48:45 +0800

    断线重连

--- public/static/service/js/laykefu.js
+++ public/static/service/js/laykefu.js
@@ -7,7 +7,7 @@ var uinfo = {
 
 // 创建一个Socket实例
 var protocol = location.protocol === 'https:' ? 'wss://' : 'ws://';
-socket = new WebSocket(protocol + socket_server);//创建Socket实例// 打开Socket 
+socket = new ReconnectingWebSocket(protocol + socket_server);//创建Socket实例// 打开Socket 
 socket.onopen = function (res) {
     layui.use(['layer'], function () {
         var layer = layui.layer;
